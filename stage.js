1d94a4d1693eea5bb032c01e12a52b8f184d3377
Wyatt Lipscomb <35582368+WyattSL@users.noreply.github.com>
2020-02-25 10:06:50 -0500

    Create stage.js

---
+++ stage.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+class Stage() {
+  constructor(token) {
+    this.token = token;
+  }
+  async 
+}
+
+module.exports = Stage;
